d6d20cc8c5f748beb213da81c87fe8ec1ebcc723
jrussell-OSU <jacob.r.russell@gmail.com>
2023-03-15 19:59:04 -0400

    feat: added skills, WIP navbar

---
+++ src/MySkills.jsx
@@ -0,0 +1,28 @@
+
+
+
+const MySkills = () => {
+
+  return (
+    <div class="skills">
+      <ul class="skills-list">
+        <li class="skill">Python</li>
+        <li class="skill">C/C++</li>
+        <li class="skill">Javascript</li>
+        <li class="skill">Linux</li>
+        <li class="skill">HTML/CSS</li>
+        <li class="skill">Google App Engine</li>
+        <li class="skill">Google Cloud Platform</li>
+        <li class="skill">RESTful APIs</li>
+        <li class="skill">Microservices</li>
+        <li class="skill">Flask</li>
+        <li class="skill">AJAX</li>
+        <li class="skill">SQL/noSQL</li>
+        <li class="skill">Git</li>
+        <li class="skill">Agile</li>
+      </ul>
+    </div>
+  );
+};
+
+export default MySkills;
